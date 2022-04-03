83355a5f4055131721d436b4ce207389d4cff0ee
Robert Ebafua <robertebafua@gmail.com>
2022-04-03 21:21:12 +0100

    Complete Typeorm Setup

---
+++ src/migrations/1649017217521-usersTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class usersTable1649017217521 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/ormconfig.ts
@@ -0,0 +1,24 @@
+import { ConfigService } from '@nestjs/config';
+import { ConnectionOptions } from 'typeorm';
+
+//config is used for creating new migration with empty up and down methods
+const configService = new ConfigService();
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+  synchronize: false,
+  logging: true,
+  logger: 'file',
+  migrationsRun: true,
+  migrations: [`${__dirname}/migrations/**/*{.ts,.js}`],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export = config;
