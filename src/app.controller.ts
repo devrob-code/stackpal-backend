b9e671ea23d4a3e54491cb8d02d000bee3a7210d
Robert Ebafua <robertebafua@gmail.com>
2022-04-17 13:22:46 +0100

    Health Check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/health-check')
+  healthCheck(): { status: boolean } {
+    return this.appService.healthCheck();
+  }
 }
