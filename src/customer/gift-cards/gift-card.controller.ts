024949a9595d56f27ea38ee4e0d91db5c964443e
Robert Ebafua <robertebafua@gmail.com>
2022-08-01 20:47:57 +0100

    Gift card flow

--- src/customer/gift-cards/gift-card.controller.ts
+++ src/customer/gift-cards/gift-card.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GiftCardService } from './gift-card.service';
 
 @Controller('gift-card')
 @UseGuards(AuthGuard('jwt'))
 export class GiftCardController {
-  //constructor(private readonly giftCardController: GiftCardService) {}
+  constructor(private readonly giftCardController: GiftCardService) {}
 }
