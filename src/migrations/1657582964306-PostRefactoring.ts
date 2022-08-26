218dc98b6a31033b50b2efe375b87cc8949a4c28
Robert Ebafua <robertebafua@gmail.com>
2022-08-26 20:45:19 +0100

    Merged and cleaned up bug from Yaro code

--- src/migrations/1657582964306-PostRefactoring.ts
+++ src/migrations/1657582964306-PostRefactoring.ts
@@ -3,16 +3,20 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class PostRefactoring1657582964306 implements MigrationInterface {
   name = 'PostRefactoring1657582964306';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "wallets" ADD "address" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "wallets" ADD "private_key" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "wallets" ADD "network" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "wallets" ADD "mnemonic" character varying NOT NULL`);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "wallets" ADD "address" character varying NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "wallets" ADD "private_key" character varying NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "wallets" ADD "network" character varying NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "wallets" ADD "mnemonic" character varying NOT NULL`,
+    );
+  }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }
